64ef63dd9c9eb7963f89d79789853c701a906072
dspham <dsvpham@gmail.com>
2019-01-15 07:47:42 +0000

    removed print statements

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,6 @@ from datetime import datetime
 import os
 
 env.hosts = ["35.237.214.104", "34.73.23.247"]
-env.user = "ubuntu"
 
 
 def do_deploy(archive_path):
@@ -14,9 +13,7 @@ def do_deploy(archive_path):
     to the web servers"""
     if not os.path.exists(archive_path):
         return False
-    print("Working0")
     put(archive_path, "/tmp/")
-    print("Working2")
     filename_with_ext = archive_path.split("/")[1]
     filename_wo_ext = filename_with_ext.split(".")[0]
     archive_dest = "/data/web_static/releases/{}".format(filename_wo_ext)
@@ -27,5 +24,4 @@ def do_deploy(archive_path):
     run("sudo rm -rf {}/web_static".format(archive_dest))
     run("sudo rm -rf /data/web_static/current")
     run("sudo ln -s {} /data/web_static/current".format(archive_dest))
-    print("WorkingFinal")
     return True
