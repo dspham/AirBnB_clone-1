34c440051fec37bc5e6b8cc7a94067a892d727c0
dspham <dsvpham@gmail.com>
2019-01-15 07:36:51 +0000

    made adjustments for styling

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ import os
 env.hosts = ["35.237.214.104", "34.73.23.247"]
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
     """Distributes the archive
     to the web servers"""
@@ -28,19 +29,3 @@ def do_deploy(archive_path):
     run("sudo ln -s {} /data/web_static/current".format(archive_dest))
     print("WorkingFinal")
     return True
-
-
-
-
-# returns false if path doesn't exist
-
-# upload the archive to the folder /data/web_static_releases/<archive filename without extension> on web server
-# delete the archive from the web server
-# delete the symbolic link /data/web_static/current from the web server
-# create a new symbolic link /data/web_static/current on the web server, linked to the new version of your code
-#     /data/web_static/releases/<archive filename without extension>
-
-# all remote commands must be executed on both web servers
-#     env.hosts = ['<IP web-01>', 'IP web-02']
-#     Returns True if all operations have been done correctly, else False
-# Use this script to deploy it on your servers: xx-web-01, xx-web-02
