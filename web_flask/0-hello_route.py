b1126d752626d5e2888c708979c701caca0c4ef8
dspham <dsvpham@gmail.com>
2019-01-31 00:47:23 +0000

    added descriptions

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+"""Hello Flask!
+Starts a web flask application"""
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
+    """displays Hello HBNB!"""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
