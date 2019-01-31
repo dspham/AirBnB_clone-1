4ac70f68cbad482abd74e51c84663ca2c9ac95c8
dspham <dsvpham@gmail.com>
2019-01-31 04:22:03 +0000

    adjusted styling

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -2,17 +2,21 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -2,30 +2,37 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     return "HBNB"
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     return "C {}".format(text.replace("_", " "))
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text="is cool"):
     return "Python {:s}".format(text.replace("_", " "))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number_check(n):
     return "{:d} is a number".format(int(n))
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     return render_template("5-number.html", n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
