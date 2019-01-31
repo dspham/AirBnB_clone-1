9f8a87bda0cb7252ea3c89ae85fcd847987477a5
dspham <dsvpham@gmail.com>
2019-01-31 04:18:14 +0000

    added descriptions

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+"""HBNB
+Starts a web flask application"""
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """displays Hello HBNB!"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """returns HBNB"""
     return "HBNB"
 
 if __name__ == "__main__":
